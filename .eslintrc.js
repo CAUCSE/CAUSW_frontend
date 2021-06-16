8f07d95b268ea67bd5e963f195765e782abc332e
Yongjun Kim <staysharp0@gmail.com>
2021-06-16 20:47:48 +0900

    chore: prettier, eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+};
