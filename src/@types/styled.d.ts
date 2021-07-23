9f6eef7136006438eec83e3d3be5416ff81c6d60
Yongjun Kim <staysharp0@gmail.com>
2021-07-23 20:05:48 +0900

    feat: 텍스트 서브 색상 추가

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -5,6 +5,7 @@ declare module 'styled-components' {
     color: {
       main: string;
       text: string;
+      textSub: string;
     };
   }
 }
--- src/styles/themes/theme-default.ts
+++ src/styles/themes/theme-default.ts
@@ -4,5 +4,6 @@ export const defaultTheme: DefaultTheme = {
   color: {
     main: '#312ED7',
     text: '#383743',
+    textSub: '#A3A1A1',
   },
 };
