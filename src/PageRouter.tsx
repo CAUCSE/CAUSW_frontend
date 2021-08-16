ddb3b1152edfc2a232a8548a494a6b9f5ebf9d49
YongJunKim <Staysharp0@gmail.com>
2021-08-16 17:58:56 +0900

    feat: 메인 페이지 목업 추가 (#5)
    * chore: CRA 레거시 파일 삭제
    
    * reafact: 라우터 분리
    
    * feat: 메인페이지 추가 및 페이지 라우트 인덱싱 정리
    
    * feat: 모바일 레이아웃 추가 및 간단한 영역 분배
    
    * chore: 레거시 코드 제거

--- src/PageRouter.tsx
+++ src/PageRouter.tsx
@@ -1,43 +1,20 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { PAGE_URL } from './configs/path';
-import { useRootStore } from './stores/RootStore';
-import { PageSignIn } from './pages/signIn';
-import { PageSignUp } from './pages/signUp';
+import { AuthRouter } from './AuthRouter';
+import * as Page from './pages';
 
 export const PageRouter: React.FC = () => {
   return (
     <Router basename={PAGE_URL.Main}>
       <Switch>
-        <Route path={PAGE_URL.SignIn} exact component={PageSignIn} />
-        <Route path={PAGE_URL.SignUp} exact component={PageSignUp} />
+        <Route path={PAGE_URL.SignIn} exact component={Page.SignIn} />
+        <Route path={PAGE_URL.SignUp} exact component={Page.SignUp} />
 
         <Route path={PAGE_URL.Err404} exact component={() => <>404</>} />
         <AuthRouter>
-          <Route path={PAGE_URL.Main} component={() => <>메인 페이지 당!</>} />
+          <Route path={PAGE_URL.Main} component={Page.Main} />
         </AuthRouter>
       </Switch>
     </Router>
-  );
-};
-
-const AuthRouter: React.FC = ({ children, ...rest }) => {
-  const { auth } = useRootStore();
-
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        auth.isSignIn ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: PAGE_URL.SignIn,
-              state: { from: location },
-            }}
-          />
-        )
-      }
-    />
   );
 };
