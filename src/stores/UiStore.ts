49268a7825dc3a5b9757699cce912d221b7c0ac8
Yongjun Kim <staysharp0@gmail.com>
2021-09-16 21:26:46 +0900

    feat: 디스플레이 타입 체킹 시스템 추가

--- src/stores/UiStore.ts
+++ src/stores/UiStore.ts
@@ -1,11 +1,43 @@
 import { HeaderUiStore } from 'components/common/header';
+import { action, makeObservable, observable } from 'mobx';
+
+enum DISPLAY {
+  MOBILE = 480,
+  TABLET = 768,
+  DESKTOP = 1024,
+}
 
 export class UiStore {
   rootStore: Store.Root;
   header: HeaderUiStore;
+  displayType: DISPLAY;
 
   constructor(rootStore: Store.Root) {
+    makeObservable(this, {
+      displayType: observable,
+      setDisplayType: action.bound,
+    });
+
     this.rootStore = rootStore;
     this.header = new HeaderUiStore();
+    this.displayType = DISPLAY.MOBILE;
+
+    this.initWindowMatchMedia();
+  }
+
+  private initWindowMatchMedia() {
+    this.setDisplayType();
+
+    window.matchMedia(`screen and (max-width: ${DISPLAY.MOBILE}px)`).addEventListener('change', this.setDisplayType);
+    window.matchMedia(`screen and (max-width: ${DISPLAY.TABLET}px)`).addEventListener('change', this.setDisplayType);
+    window.matchMedia(`screen and (min-width: ${DISPLAY.TABLET}px)`).addEventListener('change', this.setDisplayType);
+  }
+
+  setDisplayType(): void {
+    const { innerWidth } = window;
+
+    if (innerWidth <= DISPLAY.MOBILE) this.displayType = DISPLAY.MOBILE;
+    else if (innerWidth <= DISPLAY.TABLET) this.displayType = DISPLAY.TABLET;
+    else this.displayType = DISPLAY.DESKTOP;
   }
 }
