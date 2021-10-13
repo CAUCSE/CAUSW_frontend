c178cc801452e4d979bb2eb0270911d3325d8f9f
Yongjun Kim <staysharp0@gmail.com>
2021-10-13 20:19:26 +0900

    chore: 서클 빈페이지로 수정

--- src/pages/Circle/PageCircle.tsx
+++ src/pages/Circle/PageCircle.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
 
 export const PageCircle: React.FC = React.memo(() => {
-  return (
-    <>
-      <div>logo</div>
-      <div>상단 버튼 목록</div>
-      <div>배너 슬라이드</div>
-      <h2>즐겨찾는 게시판</h2>
-      <div>게시판 컴포넌트 1</div>
-      <div>게시판 컴포넌트 2</div>
-      <div>게시판 컴포넌트 3</div>
-    </>
-  );
+  return <></>;
 });
