ce0e1c458c587f7c9047798fe49bc7ab04a3e95b
Yongjun Kim <staysharp0@gmail.com>
2022-02-05 18:18:33 +0900

    feat(project): import 순서 지정 린트 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,32 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
   ],
   plugins: ['prettier', '@typescript-eslint'],
   parserOptions: {
     parser: '@typescript-eslint/parser',
+  },
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'angular',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
   },
   settings: {
     'import/resolver': {
