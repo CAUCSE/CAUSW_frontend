d57c2ae8c7fa264df5dad9d31b11746cce7105d7
rbgksqkr <rbgks1937@naver.com>
2024-01-12 13:28:05 +0900

    fix: svg 파일 리액트 컴포넌트로 가져오기 (#29)

--- src/components/Gnb/GnbContainer.tsx
+++ src/components/Gnb/GnbContainer.tsx
@@ -1,12 +1,10 @@
 import { Nav, NavLink } from './GnbView';
 
-// import { Icons } from '@/assets';
 import { PAGE_URL } from '@/configs/path';
-// import { HomeIcon } from '@/assets/icons';
-import HomeIcon from '@/assets/icons/nav_home.svg';
-import CircleIcon from '@/assets/icons/nav_circle.svg';
-import BoardIcon from '@/assets/icons/nav_board.svg';
-import SettingIcon from '@/assets/icons/nav_setting.svg';
+import HomeIcon from '@/assets/icons/nav_home.svg?react';
+import CircleIcon from '@/assets/icons/nav_circle.svg?react';
+import BoardIcon from '@/assets/icons/nav_board.svg?react';
+import SettingIcon from '@/assets/icons/nav_setting.svg?react';
 
 export const GnbContainer: React.FC = () => (
   <Nav>
