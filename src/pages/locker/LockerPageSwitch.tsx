0edd23546a2f798e8ff3b4f614ec2c37e2d2f302
selfishAltruism <officialkyus@gmail.com>
2024-01-18 03:05:09 +0900

    fix: enum PAGE_URL.LockerList 곂침 error 해결

--- src/pages/locker/LockerPageSwitch.tsx
+++ src/pages/locker/LockerPageSwitch.tsx
@@ -8,6 +8,6 @@ import { PAGE_URL } from '@/configs/path';
 export const LockerPageSwitch: React.FC = () => (
   <Switch>
     <Route path={PAGE_URL.LockerLocation} component={LockerLocations} />
-    <Route path={PAGE_URL.LockerList} component={LockerList} />
+    <Route path={PAGE_URL.Locker} component={LockerList} />
   </Switch>
 );
