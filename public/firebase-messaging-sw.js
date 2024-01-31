aa7afb7a5180c731344c355947d7e027b566ba09
rbgksqkr <rbgks1937@naver.com>
2024-01-31 16:58:43 +0900

    chore: FCM 설정값 환경변수로 가져오기 (#53)

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,12 +19,6 @@ self.addEventListener('push', function (e) {
     tag: resultData.tag,
   };
 
-  console.log(resultData.title, {
-    body: resultData.body,
-    icon: resultData.image, // 웹 푸시 이미지는 icon
-    tag: resultData.tag,
-  });
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
