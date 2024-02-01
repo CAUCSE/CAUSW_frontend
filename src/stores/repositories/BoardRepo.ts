65a39ce6f95c30dbbd0a9e45ff9904b1b807d409
rbgksqkr <rbgks1937@naver.com>
2024-02-01 22:47:16 +0900

    feat: 게시판 생성, 수정, 삭제, 복구 API 검증 및 구현 (#57)

--- src/stores/repositories/BoardRepo.ts
+++ src/stores/repositories/BoardRepo.ts
@@ -12,6 +12,24 @@ class BoardRepo {
       .filter(content => content.isDeleted === false)
       .map(({ id, category, name }) => new BoardModel(id, category, name));
   };
+
+  create = async (body: Board.CreateRequestDto): Promise<BoardModel> => {
+    const { data } = await API.post<Board.CreateResponseDto>(this.URI, body);
+    const { id, category, name } = data;
+    return new BoardModel(id, category, name);
+  };
+
+  update = async (boardId: string, body: Board.UpdateRequestDto): Promise<void> => {
+    await API.put(`${this.URI}/${boardId}`, body);
+  };
+
+  delete = async (boardId: string): Promise<void> => {
+    await API.delete(`${this.URI}/${boardId}`);
+  };
+
+  restore = async (boardId: string): Promise<void> => {
+    await API.put(`${this.URI}/${boardId}`);
+  };
 }
 
 export const BoardRepoImpl = new BoardRepo();
