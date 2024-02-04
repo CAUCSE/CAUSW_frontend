d86d1e7f5640e4d2c88b22b29fc66a8273258476
selfishAltruism <officialkyus@gmail.com>
2024-02-04 23:44:25 +0900

    feat: lockerRouterTester 적용 (#59)

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,7 +1,9 @@
 import { test } from '@playwright/test';
 
+import { lockerRouterTester } from './tester/lockerTester';
 import { settingRouterTester } from './tester/settingTester';
 import { signinTester } from './tester/signinTester';
 
 test.beforeEach(signinTester);
 test('setting', settingRouterTester);
+test('locker', lockerRouterTester);
