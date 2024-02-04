6c9cf6b0843b0a978b18f8eb1d31f178e0c00064
selfishAltruism <officialkyus@gmail.com>
2024-02-04 23:39:14 +0900

    feat: settingRouterTester 완성 (#59)

--- tests/tester/settingTester.ts
+++ tests/tester/settingTester.ts
@@ -1,12 +1,33 @@
-import { expect } from '@playwright/test';
+import { PAGE_URL } from '../../src/configs/path';
 
 export const settingRouterTester = async ({ page }) => {
+  const baseURL = 'http://localhost:3000';
+  const settingBackButton = '#root > div > header > div.css-wzsu05-Left.e1i83gr13 > a ';
+  const historyBackButton = '#root > div > header > div.css-wzsu05-Left.e1i83gr13 > button';
+
   await page.click('#root > div > nav > a:nth-child(5)');
-  await page.waitForURL('http://localhost:3000/setting');
+  await page.waitForURL(baseURL + PAGE_URL.Setting);
 
-  await page.click('#root > div > main > div > section:nth-child(2) > a:nth-child(2)');
-  await page.waitForURL('http://localhost:3000/setting/profile');
+  await page.getByText('개인정보 관리').click();
+  await page.waitForURL(baseURL + PAGE_URL.SettingProfile);
+  await page.click(settingBackButton);
+  await page.waitForURL(baseURL + PAGE_URL.Setting);
 
-  await page.click('#root > div > header > div.css-wzsu05-Left.e1i83gr13 > a > svg > g > path');
-  await page.waitForURL('http://localhost:3000/setting');
+  await page.getByText('비밀번호 변경').click();
+  await page.waitForURL(baseURL + PAGE_URL.SettingPassword);
+  await page.click(settingBackButton);
+  await page.waitForURL(baseURL + PAGE_URL.Setting);
+
+  await page.getByText('내가 쓴 글').click();
+  await page.waitForURL(baseURL + PAGE_URL.HistoryPost);
+  await page.click(historyBackButton);
+  await page.waitForURL(baseURL + PAGE_URL.Setting);
+
+  await page.getByText('내가 쓴 댓글').click();
+  await page.waitForURL(baseURL + PAGE_URL.HistoryComment);
+  await page.click(historyBackButton);
+  await page.waitForURL(baseURL + PAGE_URL.Setting);
+
+  await page.getByText('로그아웃').click();
+  await page.waitForURL(baseURL + '/auth/signin');
 };
