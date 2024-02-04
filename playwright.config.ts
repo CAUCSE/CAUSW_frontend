44a91f0f5e2d1ba0a18cf545e99f7e37c6e8af54
selfishAltruism <officialkyus@gmail.com>
2024-02-04 21:56:30 +0900

    feat: auth setup 백업, 사용 불가능 판단 (#59)

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,11 +23,9 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    storageState: 'playwright/.auth/user.json',
+    headless: false,
   },
 
   projects: [
@@ -35,19 +33,19 @@ export default defineConfig({
 
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], storageState: 'playwright/.auth/user.json' },
+      use: { ...devices['Desktop Chrome'] },
       dependencies: ['setup'],
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'], storageState: 'playwright/.auth/user.json' },
+      use: { ...devices['Desktop Firefox'] },
       dependencies: ['setup'],
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'], storageState: 'playwright/.auth/user.json' },
+      use: { ...devices['Desktop Safari'] },
       dependencies: ['setup'],
     },
 
--- tests/tester/settingTester.ts
+++ tests/tester/settingTester.ts
@@ -3,7 +3,5 @@ import { expect } from '@playwright/test';
 export const settingTester = async ({ page }) => {
   await page.goto('http://localhost:3000/home');
 
-  await page.click('#root > div > nav > a.css-1coshah-LinkStyle.e1w6r2gj0.active');
-
-  await expect(page.url() === 'http://localhost:3000/setting');
+  await expect(page.getByText('동문 네트워크')).toBeVisible();
 };
