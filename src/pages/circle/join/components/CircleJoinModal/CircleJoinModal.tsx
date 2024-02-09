9ecc0ed02735157569242ad1560557a1a1ec666f
rbgksqkr <rbgks1937@naver.com>
2024-02-09 17:52:24 +0900

    refactor: isNewUser 변수로 새로 가입하는 사용자 boolean 값 계산 (#68)

--- src/pages/circle/join/components/CircleJoinModal/CircleJoinModal.tsx
+++ src/pages/circle/join/components/CircleJoinModal/CircleJoinModal.tsx
@@ -20,6 +20,7 @@ export const CircleJoinModal: React.FC = observer(() => {
     circle,
     joinModal: { target, join, reset, visible, close, isAwait, isBlock, isDone, isMember },
   } = usePageUiStore<PageUiStore.CircleJoin>();
+  const isNewUser = !(isDone || isAwait || isBlock || isMember);
 
   const handleOk = useCallback(async () => {
     if (!target) return;
@@ -39,17 +40,16 @@ export const CircleJoinModal: React.FC = observer(() => {
           {isMember && '이미 가입한 동아리입니다.'}
           {isAwait && '이미 신청한 동아리입니다.'}
           {isBlock && '가입이 제한되었습니다.'}
-          {!(isDone || isAwait || isBlock || isMember) && '동아리 신청'}
+          {isNewUser && '동아리 신청'}
         </ModalAlertTitle>
         <ModalAlertMessage center>
           {(isDone || isAwait) && '동아리 동아리장이 가입을 허가해 줄 때까지 기다려주세요.'}
           {isMember && '이미 가입한 동아리 사용자 입니다.'}
           {isBlock && '동아리장 혹은 관리자에게 문의해주세요.'}
-          {!(isDone || isAwait || isBlock || isMember) &&
-            `${circle.name} 동아리에 가입하시겠습니까?`}
+          {isNewUser && `${circle.name} 동아리에 가입하시겠습니까?`}
         </ModalAlertMessage>
         <ModalFooter>
-          {isDone || isAwait || isBlock || isMember ? (
+          {!isNewUser ? (
             <ModalFooterButton onClick={close}>확인</ModalFooterButton>
           ) : (
             <>
