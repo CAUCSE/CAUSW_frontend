3542939b00faed541ed6f199f28af3f1d9fe2a54
selfishAltruism <officialkyus@gmail.com>
2024-02-18 16:48:46 +0900

    fix: access token 만료 이후 user/me api 응답 거부 error 재수정 (#71)

--- src/AuthRouter.tsx
+++ src/AuthRouter.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
-import { useEffect, useRef } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useHistory, Route, Redirect } from 'react-router-dom';
 
 import { PAGE_URL } from '@/configs/path';
 import { useRootStore } from '@/stores/RootStore';
@@ -13,23 +13,26 @@ export const AuthRouter: React.FC<Props> = observer(({ children, ...rest }) => {
   const {
     auth: { isSignIn, checkToken },
   } = useRootStore();
-  const tokenAvailable = useRef<boolean>(false);
+  const history = useHistory();
 
   const isTokenAvailable = async () => {
-    const { success, errorCode, message } = (await checkToken()) as unknown as StoreAPI;
-    if (success) tokenAvailable.current = true;
+    const { success } = (await checkToken()) as unknown as StoreAPI;
+    if (!success)
+      history.push({
+        pathname: PAGE_URL.SignIn,
+        state: { from: location },
+      });
   };
 
   useEffect(() => {
-    if (isSignIn && !tokenAvailable.current) isTokenAvailable();
-    console.log(tokenAvailable.current);
+    if (isSignIn) isTokenAvailable();
   }, []);
 
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        tokenAvailable.current ? (
+        isSignIn ? (
           children
         ) : (
           <Redirect
