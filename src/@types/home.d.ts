b0f65d2ae687b0f2fa65e066faccbd3271b99170
selfishAltruism <officialkyus@gmail.com>
2024-03-01 01:44:35 +0900

    fix: GetHomePageResponseDto변경(#83)

--- src/@types/home.d.ts
+++ src/@types/home.d.ts
@@ -11,6 +11,31 @@ declare namespace Home {
     };
     posts: {
       content: Post.Dto[];
+      empty: boolean;
+      first: boolean;
+      last: boolean;
+      number: number;
+      numberOfElements: number;
+      pageable: {
+        offset: number;
+        pageNumber: number;
+        pageSize: number;
+        paged: boolean;
+        sort: {
+          empty: boolean;
+          sorted: boolean;
+          unsorted: boolean;
+        };
+        unpaged: boolean;
+      };
+      size: number;
+      sort: {
+        empty: boolean;
+        sorted: boolean;
+        unsorted: boolean;
+      };
+      totalElements: number;
+      totalPages: number;
     };
   }[];
 
