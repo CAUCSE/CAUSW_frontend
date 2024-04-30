a041413effe5bd7751353b5a4e091b539fa9288a
omjinLTS <omjin7g@gmail.com>
2024-04-30 15:45:59 +0900

    refactor : 라우터 호환 패키지 설치

--- src/PageRouter.tsx
+++ src/PageRouter.tsx
@@ -1,10 +1,11 @@
 import { lazy, Suspense } from 'react';
 import {
-  BrowserRouter as RootRouter,
   Redirect,
-  Route,
+  BrowserRouter as RootRouter,
   Switch as RootSwitch,
+  Route,
 } from 'react-router-dom';
+import { CompatRouter } from 'react-router-dom-v5-compat';
 
 import { AuthRouter } from './AuthRouter';
 import Loading from './components/Loading';
@@ -17,22 +18,25 @@ const HomePage = lazy(() => import('./pages/home/HomePage'));
 export const PageRouter: React.FC = () => (
   <Suspense fallback={<Loading />}>
     <RootRouter>
-      <Route exact path="/">
-        <Redirect to={PAGE_URL.Home} />
-      </Route>
+      {/* TODO : remove after migration completed */}
+      <CompatRouter>
+        <Route exact path="/">
+          <Redirect to={PAGE_URL.Home} />
+        </Route>
 
-      <RootSwitch>
-        <Route path={PAGE_URL.Auth} component={Switch.AuthPage} />
+        <RootSwitch>
+          <Route path={PAGE_URL.Auth} component={Switch.AuthPage} />
 
-        <AuthRouter>
-          <Route path={PAGE_URL.Home} component={HomePage} />
-          <Route path={PAGE_URL.Locker} component={Switch.LockerPage} />
-          <Route path={PAGE_URL.Circle} component={Switch.CirclePage} />
-          <Route path={PAGE_URL.Board} component={Switch.BoardPage} />
-          <Route path={PAGE_URL.Setting} component={Switch.SettingPage} />
-          <Route path={PAGE_URL.History} component={Switch.HistoryPage} />
-        </AuthRouter>
-      </RootSwitch>
+          <AuthRouter>
+            <Route path={PAGE_URL.Home} component={HomePage} />
+            <Route path={PAGE_URL.Locker} component={Switch.LockerPage} />
+            <Route path={PAGE_URL.Circle} component={Switch.CirclePage} />
+            <Route path={PAGE_URL.Board} component={Switch.BoardPage} />
+            <Route path={PAGE_URL.Setting} component={Switch.SettingPage} />
+            <Route path={PAGE_URL.History} component={Switch.HistoryPage} />
+          </AuthRouter>
+        </RootSwitch>
+      </CompatRouter>
     </RootRouter>
   </Suspense>
 );
