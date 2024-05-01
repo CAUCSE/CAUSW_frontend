c78a8896631f459f68ba2c9c13dafac33d9ad4d0
selfishAltruism <officialkyus@gmail.com>
2024-05-01 17:26:59 +0900

    design: error page design (#124)

--- src/pages/auth/apiError/ApiErrorPage.tsx
+++ src/pages/auth/apiError/ApiErrorPage.tsx
@@ -4,21 +4,19 @@ import React from 'react';
 
 import { PageUiStoreImpl } from './ApiErrorPageUiStore';
 
-import { BodyScreen, Header, PageBody, PageStoreHOC } from '@/components';
+import { PageBody, PageStoreHOC, Header } from '@/components';
 import { PAGE_URL } from '@/configs/path';
 
 const ApiErrorPage: React.FC = observer(() => {
   return (
     <>
+      <Header withBack={PAGE_URL.Home} title="500 Error" />
       <PageBody>
         <Wrapper>
-          <img src="/images/empty.png" alt="Empty list logo" />
-          <br />
-          일시적으로 서비스를 사용할 수 없습니다.
-          <br />
-          해당 문제가 지속적으로 발생하는 경우
-          <br />
-          이하 이메일로 연락주세요.
+          <img src="/images/mascot-study.png" alt="Api Error Img" />
+          <h4>일시적으로 서비스를 이용할 수 없습니다.</h4>
+          해당 문제가 지속적으로 발생하는 경우 <br />
+          아래 이메일로 문의해주세요.
           <br />
           caucsedongne@gmail.com
         </Wrapper>
@@ -28,16 +26,21 @@ const ApiErrorPage: React.FC = observer(() => {
 });
 
 const Wrapper = styled.div`
-  margin: 200px 0 30px;
-  font-size: 17px;
+  margin: 110px 0 30px;
+  font-size: 14px;
   //font-weight: bolder;
   color: gray;
   line-height: 20px;
   text-align: center;
 
   > img {
+    margin-bottom: -10px;
+    width: 150px;
+  }
+
+  > h4 {
+    font-size: 15px;
     margin-bottom: 10px;
-    width: 100px;
   }
 `;
 
