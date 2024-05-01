505fdb89f37a390ebc23ee50223aaf5ec87d5805
selfishAltruism <officialkyus@gmail.com>
2024-05-01 17:30:50 +0900

    design: error page design (#124)

--- src/pages/auth/apiError/ApiErrorPage.tsx
+++ src/pages/auth/apiError/ApiErrorPage.tsx
@@ -14,9 +14,13 @@ const ApiErrorPage: React.FC = observer(() => {
       <PageBody>
         <Wrapper>
           <img src="/images/mascot-study.png" alt="Api Error Img" />
-          <h4>일시적으로 서비스를 이용할 수 없습니다.</h4>
-          해당 문제가 지속적으로 발생하는 경우 <br />
+          <h4>
+            일시적으로 서비스를 <br />
+            이용할 수 없습니다.
+          </h4>
+          문제가 지속적으로 발생하는 경우 <br />
           아래 이메일로 문의해주세요.
+          <br />
           <br />
           caucsedongne@gmail.com
         </Wrapper>
@@ -26,20 +30,19 @@ const ApiErrorPage: React.FC = observer(() => {
 });
 
 const Wrapper = styled.div`
-  margin: 110px 0 30px;
-  font-size: 14px;
+  margin: 100px 0 30px;
+  font-size: 12px;
   //font-weight: bolder;
   color: gray;
-  line-height: 20px;
   text-align: center;
 
   > img {
-    margin-bottom: -10px;
-    width: 150px;
+    margin-bottom: -5px;
+    width: 170px;
   }
 
   > h4 {
-    font-size: 15px;
+    font-size: 18px;
     margin-bottom: 10px;
   }
 `;
